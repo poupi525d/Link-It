e3270d289a435c266636cb54a9261fd601e7f834
RALAIZEFA Michael <m.ralaizefa@laposte.net>
2018-05-22 17:52:09 +0200

    Initial Commit

---
+++ Link It/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Link It
+//
+//  Created by Michaël on 22/05/2018.
+//  Copyright © 2018 Michaël. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Link It/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Link It
+//
+//  Created by Michaël on 22/05/2018.
+//  Copyright © 2018 Michaël. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
